9ddb38001f7e7b24fa6131b522bf5a9e1ed04ee2
Hanz Hendrick Lacsi <hanzlacsi@gmail.com>
2026-03-06 17:22:47 +0800

    new

--- script.js
+++ script.js
@@ -224,3 +224,18 @@ document.querySelectorAll('[data-reveal]').forEach((el, i) => {
   el.style.transitionDelay = `${i % 3 * 80}ms`;
   revealObserver.observe(el);
 });
+
+// Back to top button
+const backToTop = document.getElementById('backToTop');
+if (backToTop) {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 400) {
+      backToTop.classList.add('visible');
+    } else {
+      backToTop.classList.remove('visible');
+    }
+  });
+  backToTop.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+}
